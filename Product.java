a5cbe13cacb79172c35c2c0fa4fdcf5af2d9c42a
meritbh <meritbhusal@gmail.com>
2025-01-02 23:55:23 -0600

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,15 +1,8 @@
 package com.lockedin.myapp;
 
-import java.util.ArrayList;
+import jakarta.persistence.*;
 import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "products")
